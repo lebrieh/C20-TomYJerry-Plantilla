238aaea0b11d2a6aee0a85193f918d2bbbbc7977
lebrieh <89428782+lebrieh@users.noreply.github.com>
2021-10-28 12:44:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
+function preload() {
+   //Carga aquí las imágenes
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    // crea aquí los sprites de Tom y Jerry
+
+}
+
+function draw() {
+
+    background(bg);
+
+    //Escribe aquí la condición para evaluar si Tom y Jerry chocan
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    //escribe aquí el código para la animación de movimiento y cambio
+}
